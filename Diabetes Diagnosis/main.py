f4c40ead214578740f0d87030c0fad6d33bc7f76
Zamam-Moeez <zmoeez@outlook.com>
2022-01-12 16:46:04 -0700

    feature mapping

--- Diabetes Diagnosis/main.py
+++ Diabetes Diagnosis/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn as sk
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
 def convert_dtype(x):
@@ -10,17 +11,59 @@ def convert_dtype(x):
         return str(x)
     except:
         return ''
+def pre_process():
+    dataf = pd.read_csv('diabetes.csv',converters={'first_column': convert_dtype,'second_column': convert_dtype})
 
-dataf = pd.read_csv('diabetes.csv',converters={'first_column': convert_dtype,'second_column': convert_dtype})
+    print(dataf.head(3))
 
-inspecion = dataf.describe()
+    inspecion = dataf.describe()
 
-ds = tf.data.Dataset.from_tensor_slices(dict(dataf))
+    print(dataf.columns)
 
-print(ds)
+    columns = ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin",
+       "BMI", "DiabetesPedigreeFunction", "Age", "Outcome"]
 
-for elem in ds:
-    print(elem)
+    fet_colm = ["preg_f"]
+
+    datanorm  = dataf.iloc[: , :8] #only need features, drop the Outcome for now
+
+    datanorm=(datanorm-datanorm.min())/(datanorm.max()-datanorm.min())
+
+    print(datanorm)
+
+    inp = datanorm
+
+    oup = df["Outcome"]
+
+    inp_tr, , inp_te, oup_tr, oup_te = train_test_split(inp,oup, test_size=0.2,random_state=101)
+
+    return  inp_tr, , inp_te, oup_tr, oup_te
+
+
+def features():
+
+    preg_f = tf.feature_column.numeric_column("Pregnancies")
+
+    gluc_f = tf.feature_column.numeric_column("Glucose")
+
+    bl_f = tf.feature_column.numeric_column("BloodPressure")
+
+    sk_f = tf.feature_column.numeric_column("SkinThickness")
+
+    ins_f = tf.feature_column.numeric_column("Insulin")
+
+    bmi_f = tf.feature_column.numeric_column("BMI")
+
+    DPF_f = tf.feature_column.numeric_column("DiabetesPedigreeFunction")
+
+    age_f = tf.feature_column.numeric_column("Age")
+
+    fetcol = [preg_f, gluc_f, bl_f, sk_f, ins_f, bmi_f, DPF_f, age_f]
+
+    return fetcol
+
+
+
 
 
 
